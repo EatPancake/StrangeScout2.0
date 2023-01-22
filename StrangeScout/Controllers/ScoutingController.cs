9f556ac13283f16dbf9a83920b0ed4b0958b01a0
EatPancake <104321961+EatPancake@users.noreply.github.com>
2023-01-22 14:24:08 -0500

    Add project files.

---
+++ StrangeScout/Controllers/ScoutingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StrangeScout.Controllers
+{
+    public class ScoutingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Input()
+        {
+            return View();
+        }
+    }
+}
---
+++ StrangeScout/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StrangeScout.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
