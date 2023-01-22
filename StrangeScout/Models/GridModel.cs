39678e45fe94c8f9c1ad677b9d828fa146345e7f
EatPancake <104321961+EatPancake@users.noreply.github.com>
2023-01-22 14:40:35 -0500

    Added start of models

---
+++ StrangeScout/Models/GridModel.cs
@@ -0,0 +1,26 @@
+﻿namespace StrangeScout.Models
+{
+    public class GridModel
+    {
+        public bool r1c1 { get; set; }
+        public bool r1c2 { get; set; }
+        public bool r1c3 { get; set; }
+        public bool r1c4 { get; set; }
+        public bool r1c5 { get; set; }
+        public bool r1c6 { get; set; }
+
+        public bool r2c1 { get; set; }
+        public bool r2c2 { get; set; }
+        public bool r2c3 { get; set; }
+        public bool r2c4 { get; set; }
+        public bool r2c5 { get; set; }
+        public bool r2c6 { get; set; }
+
+        public bool r3c1 { get; set; }
+        public bool r3c2 { get; set; }
+        public bool r3c3 { get; set; }
+        public bool r3c4 { get; set; }
+        public bool r3c5 { get; set; }
+        public bool r3c6 { get; set; }
+    }
+}
---
+++ StrangeScout/Models/TeamsModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StrangeScout.Models
+{
+    public class TeamsModel
+    {
+        [Required(ErrorMessage ="team# required")]
+        public int TeamNumber { get; set; }
+        [Required(ErrorMessage = "team name required")]
+        public string TeamName { get; set; }
+        [Required(ErrorMessage = "location required")]
+        public string Location { get; set; }
+        public string District { get; set; }
+        [Required(ErrorMessage = "Rookie year required")]
+        public int RookieYear { get; set; }
+    }
+}
