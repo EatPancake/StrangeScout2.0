78ffa28af897e074878cda9a778cde867db0dcde
Pancake <eatpancake1@gmail.com>
2023-03-02 05:59:25 -0800

    week 1

--- StrangeScout/Program.cs
+++ StrangeScout/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using StrangeScout.Data;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<StrangeScoutContext>(options =>
     options.UseSqlite("Data Source=StrangeScoutContext-8513e199-7def-4329-8d0f-1f297ddc90e8.db"));
@@ -10,14 +13,20 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
+//if (!app.Environment.IsDevelopment())
+//{
+   // app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+   // app.UseHsts();
+//}
 
-app.UseHttpsRedirection();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{ 
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
